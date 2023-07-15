177e44b7922a8eababdab86a86f960b45f5d9ec6
Sagar-Krish1305 <202201139@daiict.ac.in>
2023-07-15 11:05:31 +0530

    Algorithms

--- Codeforces2/x.cpp
+++ Codeforces2/x.cpp
@@ -2,10 +2,27 @@
 #include<cmath>
 using namespace std;
 int main(){
+    int t;
+    cin >> t;
+    while (t--))
+    {
+        int q;
+        int queries[q];
+        int max = -1;
+        for(int i = 0 ; i < q ; i++){
+            cin >> queries[i];
+            if(queries[i] >= max){
+                max = queries[i];
+                cout << "1";
+            }else{
+                if(queries[i] <= queries[0]){
+                    cout << "1";
+                }else{
+                    cout << "0";
+                }
+            }
+        }
+        cout << endl;
+    }
     
-   int x,y;
-   cin>>x>>y;
-   if(2*x == 3*y){
-       cout << 2*x+2 << endl; 
-   }else{cout << max(x*2,y*3) << endl;}
 }
