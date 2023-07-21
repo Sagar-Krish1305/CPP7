d866c73cf537a44b167069eaade4d869f97a8fa0
Sagar-Krish1305 <202201139@daiict.ac.in>
2023-07-21 11:58:05 +0530

    Code Forces

--- Codeforces2/x.cpp
+++ Codeforces2/x.cpp
@@ -1,14 +1,52 @@
+ /*input
+3
+3
+4
+1 2 4 8
+3
+2 9 1
+2
+1 4
+*/
 #include<iostream>
 #include<cmath>
+#include<set>
+#include<unordered_map>
 using namespace std;
+
+char invertChar(char c){
+    if(c >= 'a' && c <= 'z'){
+        return c - 'a' + 'A';
+    }
+    return c + 'a' - 'A';
+}
 int main(){
     int t;
     cin >> t;
     while (t--){
-        int N;
-        cin >> N;
-        long long x = (N+1)/2 - 1;
-        cout << 8*x*(x+1)*(2*x + 1)/6 << endl;
+        int m;
+        cin >> m;
+        vector<int> v(50000);
+        for(int i = 0 ; i < m ; i++){
+            int n;
+            cin >> n;
+            
+            int k;
+            for(int j = 0 ; j < n ; j++){
+                cin >> k;
+                v[k] = i;
+            }
+        }
+
+        sort(v.begin(),v.end(),greater<int>());
+        int i;
+        for(i = 0 ; i < v.size() ; i++){
+            if(v[i]==0) break;
+        }
+        for(int j = i ; j >= 0; j--){
+            cout << v[j] << " ";
+        }
+        cout << endl;
         
     }
     
