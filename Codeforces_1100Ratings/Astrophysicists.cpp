bee187c3051e21cc2cbdf34b357c76235cb5d959
Sagar-Krish1305 <202201139@daiict.ac.in>
2023-08-08 23:18:36 +0530

    Codeforces

---
+++ Codeforces_1100Ratings/Astrophysicists.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k, g;
+        cin >> n >> k >> g;
+        //-->/////////////////<--
+        /*
+            let 
+        */
+        int rmax = g/2;
+        cout << rmax*(n-1)
+
+        //What if there are not enough coins? will there be any more coins?
+        
+
+
+    }
+    
+
+  return 0;
+}
--- Codeforces_1100Ratings/Minimize_Permutation_Subarrays.cpp
+++ Codeforces_1100Ratings/Minimize_Permutation_Subarrays.cpp
@@ -8,7 +8,7 @@ int main(){
         int n;
         cin >> n;
         int arr[n];
-        int indexOf1 = -1, indexOf2 = -1;
+        int indexOf1 = -1, indexOf2 = -1, indexOfN = -1;
         for(int i = 0 ; i < n ; i++){
             cin >> arr[i];
             if(arr[i] == 1){
@@ -17,17 +17,25 @@ int main(){
             if(arr[i] == 2){
                 indexOf2 = i;
             }
+            if(arr[i] == n){
+                indexOfN = i;
+            }
         }
         /////////////
-        /*  
-            if i have index of 1 and 2
-            i can seperate them;
-            if(i1 > i2){
-                return max(i2  )
-            }else{ // i1 < i2 
-                return max(i2 ,)
-            }
-        */
+        if(indexOf2 < indexOfN && indexOfN < indexOf1){
+            cout << indexOfN+1 << " " << indexOfN+1 << endl;    
+        }else if(indexOf1 < indexOfN && indexOfN < indexOf2){
+            cout << indexOfN+1 << " " << indexOfN+1 << endl;
+        }else if(indexOfN < indexOf1 && indexOf1 < indexOf2){
+            cout << indexOfN+1 << " " << indexOf1+1 << endl;
+        }else if(indexOfN < indexOf2 && indexOf2 << indexOf1){
+            cout << indexOfN+1 << " " << indexOf2+1 << endl;
+        }else if(indexOfN > indexOf1 && indexOf1 > indexOf2){
+            cout << indexOfN+1 << " " << indexOf1+1 << endl;
+        }else{
+            cout << indexOfN+1 << " " << indexOf2+1 << endl;
+        }
+       
     }
     
     
